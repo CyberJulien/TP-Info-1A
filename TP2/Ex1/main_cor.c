1081fc71786da4cb79a3525a1ce756ce581facb2
CyberJulien <j.smadja56785@gmail.com>
2024-01-26 08:49:31 +0100

    Working recursive program

--- TP2/Ex1/main_cor.c
+++ TP2/Ex1/main_cor.c
@@ -9,9 +9,9 @@ int pascal(int i, int j)
     }    
     if (i == 0 && j != 0)
     {
-        return(1);
+        return(0);
     }
-    return(pascal(i-1,j)+pascal(i-1,j-1));
+    return(pascal(i-1,j) + pascal(i-1,j-1));
 }
 
 void affiche_pascal (int N)
@@ -20,7 +20,7 @@ void affiche_pascal (int N)
     {
         for(int colonne = 0; colonne <= ligne; colonne++)
         {
-            printf("%i", pascal(ligne,colonne));
+            printf("%i ", pascal(ligne,colonne));
         }
         printf("\n");
     }
